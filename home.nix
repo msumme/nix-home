1783083e237e4fcf9e7643a8a615e34ce57cfd3c
Max Summe <max.summe@hover.to>
2021-05-21 17:13:16 -0700

    Now does stuff

---
+++ home.nix
@@ -0,0 +1,32 @@
+{ config, pkgs, ... }:
+let
+  myPackages = (with pkgs; [ nixfmt direnv diff-so-fancy ]);
+  homeDirectory = /Users/msumme;
+  varFile = homeDirectory + "/hm-vars.nix";
+  vars = import varFile;
+in {
+  programs.home-manager.enable = true;
+  home.username = "msumme";
+  home.homeDirectory = homeDirectory;
+  home.stateVersion = "21.05";
+
+  home.packages = [ ] ++ myPackages;
+
+  programs = {
+    vim = {
+      enable = true;
+      plugins = [pkgs.vimPlugins.vim-nix];
+      extraConfig = ''
+set nocompatible
+syntax enable
+set number
+set softtabstop=2 expandtab
+set incsearch
+set shiftwidth=4
+map <F2> :set number!<CR>
+imap <F2> <C-o>:set number!<CR>
+'';
+    };
+    git = import ./git.nix {inherit (vars) git_name git_email;};
+  };
+}
--- install.sh
+++ install.sh
@@ -1,14 +1,41 @@
 #!/bin/bash
 
-# install nix
-if ! command -v nix-channel; then
-    sh <(curl -L https://nixos.org/nix/install) --darwin-use-unencrypted-nix-store-volume
-    source "$HOME/.nix-profile/etc/profile.d/nix.sh"
-fi
+main() {
+  check_preconditions
+  install_nix_if_missing
+  install_home_manager
+  link_config
+  enable_homemanager
+}
 
-# install home-manager
-nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-nix-channel --update
+check_preconditions() {
+  if [[ ! -f "$HOME/hm-vars.nix" ]]; then
+    echo
+    >&2 echo "ERROR: You need to copy hm-vars.nix.example to $HOME/hm-vars.nix and set the appropriate values"
+    exit 1
+  fi
+}
 
+install_nix_if_missing() {
+  if ! command -v nix-channel; then
+      sh <(curl -L https://nixos.org/nix/install) --darwin-use-unencrypted-nix-store-volume
+      source "$HOME/.nix-profile/etc/profile.d/nix.sh"
+  fi
+}
 
-nix-shell '<home-manager>' -A install
+install_home_manager() {
+  nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+  nix-channel --update
+
+  nix-shell '<home-manager>' -A install
+}
+
+link_config() {
+  ln -sf "$PWD/home.nix" "$HOME/.config/nixpkgs/home.nix"
+}
+
+enable_homemanager() {
+  home-manager switch
+}
+
+main
